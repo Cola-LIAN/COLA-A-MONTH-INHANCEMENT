e03c2f88c92e795c0568ffd9922e058f518e02ab
Cola LIAN <Suran_Lian@epam.com>
2019-12-23 19:04:12 +0800

    update aircraft carrier

--- 12.20/aircraft-carrier-class-inheritance.js
+++ 12.20/aircraft-carrier-class-inheritance.js
@@ -6,6 +6,12 @@ class Aircraft{
         this.ammunition = ammunition;
         this.baseDamage = baseDamage;
         this.maxAmmo = maxAmmo;
+        // this.status = {
+        //     'Type': this.type,
+        //     'Ammo': this.ammunition,
+        //     'Base Damage': this.baseDamage,
+        //     'All Damage': this.baseDamage * this.ammunition,
+        // }
     }
 
     fight(){
@@ -15,8 +21,9 @@ class Aircraft{
     }
 
     refill(ammo){
-        this.ammunition = Math.min(this.maxAmmo, this.ammunition + ammo);
-        return this.ammunition == this.maxAmmo ? 1 : 'error'
+        let refilled =  Math.min(this.maxAmmo - this.ammunition, ammo);
+        this.ammunition += refilled;
+        return refilled
     }
 
     getType(){
@@ -24,11 +31,12 @@ class Aircraft{
     }
 
     getStatus(){
+        // console.log(this.status);
         return {
             'Type': this.type,
             'Ammo': this.ammunition,
             'Base Damage': this.baseDamage,
-            'All Damage': this.fight(),
+            'All Damage': this.baseDamage * this.ammunition,
         }
     }
 
@@ -51,22 +59,57 @@ class Carrier{
     }
 
     fill(ammo){
-
         this.aircrafts.forEach(aircraft => {
-            while(ammo > 0){
-                if(aircraft.isPriority){
-                    aircraft.refill(ammo);
+            if(aircraft.isPriority()){
+                if(ammo > 0){
+                    ammo -= aircraft.refill(ammo);               
                 }
             }
         })
-
         this.aircrafts.forEach(aircraft => {
-            while(ammo > 0){
-                if(!aircraft.isPriority){
-                    aircraft.refill(ammo);
+            if(!aircraft.isPriority()){
+                if(ammo > 0){
+                    ammo -= aircraft.refill(ammo);               
                 }
             }
         })
-
     }
+
+    flight(enemy){
+        this.aircrafts.forEach(aircraft => {
+            aircraft.fight();
+            this.healthyPoint = 0;
+        })
+    }
+
+    getStatus(){
+        this.aircrafts.map(aircraft => {
+            console.log(aircraft.getStatus());
+        })
+    }
+
 }
+
+let airCraft1 = new Aircraft("F16", 2, 30, 8);
+let airCraft2 = new Aircraft("F35", 5, 50, 12);
+let airCraft3 = new Aircraft("F16", 4, 30, 8);
+let airCraft4 = new Aircraft("F35", 6, 50, 12);
+
+let carrier1 = new Carrier(healthyPoint = 5000);
+carrier1.add(airCraft1);
+carrier1.add(airCraft2);
+carrier1.add(airCraft3);
+
+let carrier2 = new Carrier(healthyPoint = 4000);
+carrier2.add(airCraft3);
+carrier2.add(airCraft4);
+
+
+//test for carrier's fill function
+// carrier1.getStatus();
+// carrier1.fill(10);
+// carrier1.getStatus();
+
+carrier2.getStatus();
+carrier2.fill(8);
+carrier2.getStatus();
---
+++ 12.20/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+r(healthyPoint = 4000);
+// carrier2.add(airCraft3);
+// carrier2.add(airCraft4);
